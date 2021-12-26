4c81f6c60c5390eca035302e8101551e3a8b566d
prenger <bjoern.prenger@kps.com>
2021-12-26 21:55:18 +0100

    refactor keyboard input functions

--- src/Search.svelte
+++ src/Search.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { tick, onMount } from 'svelte';
-  import { activeSection, sectionNavigationActive, inputStatus } from './stores.js';
+  import { activeSection, sectionNavigationActive, inputStatus, delayShort } from './stores.js';
 
   import type { InputStatus } from './types/input.status.js';
 
@@ -13,53 +13,101 @@
   ];
 
   let keyboardBlocked = false;
+  let keyboardX = 0;
+  let keyboardY = 0;
   let keyboardBlocker: NodeJS.Timeout;
   let keyboardActive = false;
 
-
   let searchInput: HTMLInputElement;
+  let searchTerm = '';
 
   async function checkInput(inputStatus: InputStatus):Promise<void> {
     if ($activeSection !== sectionIdx && searchInput) {
-      await tick();
       searchInput.blur();
-      keyboardActive = false;
+      activateKeyboard(false);
     } else if (inputStatus.buttonA && !keyboardActive) {
+      blockKeyboard();
+      activateKeyboard(true);
+    } else if (inputStatus.buttonA && keyboardActive && !keyboardBlocked) {
+      blockKeyboard();
       await tick();
-      keyboardActive = true;
-      sectionNavigationActive.set(false);
+      searchTerm += keyLines[keyboardY][keyboardX];
     } else if (inputStatus.buttonB && keyboardActive) {
+      activateKeyboard(false);
+    } else if (inputStatus.up && keyboardActive && !keyboardBlocked) {
+      blockKeyboard();
       await tick();
-      keyboardActive = false;
-      sectionNavigationActive.set(true);
-    }
+      keyboardY --;
+      if (keyboardY < 0) {
+        keyboardY = keyLines.length - 1;
+      }
+      adjustColIndex();
+    } else if (inputStatus.down && keyboardActive && !keyboardBlocked) {
+      blockKeyboard();
+      await tick();
+      keyboardY ++;
+      if (keyboardY === keyLines.length) {
+        keyboardY = 0;
+      }
+      adjustColIndex();
+    } else if (inputStatus.left && keyboardActive && !keyboardBlocked) {
+      blockKeyboard();
+      await tick();
+      keyboardX --;
+      if (keyboardX < 0) {
+        keyboardX = keyLines[keyboardY].length - 1;
+      }
+    } 
+    else if (inputStatus.right && keyboardActive && !keyboardBlocked) {
+      blockKeyboard();
+      await tick();
+      keyboardX ++;
+      if (keyboardX === keyLines[keyboardY].length) {
+        keyboardX = 0;
+      }
+    } 
   }
 
-  function blockNavigation(activeSection: number): void {
+  async function activateKeyboard(active: boolean):Promise<void> {
+    await tick();
+      keyboardActive = active;
+      sectionNavigationActive.set(!active);
+  }
+
+  function adjustColIndex(): void {
+    if (keyboardX >= keyLines[keyboardY].length) {
+        keyboardX = keyLines[keyboardY].length - 1;
+      }
+  }
+
+  function blockKeyboard(): void {
+    keyboardBlocked = true;
+    keyboardBlocker = setTimeout( () => keyboardBlocked = false, $delayShort);
+  }
+
+  function setActive(activeSection: number): void {
     if (activeSection === sectionIdx && searchInput) {
       searchInput.focus();
     }
   }
 
   $: checkInput($inputStatus);
-  $: blockNavigation($activeSection);
+  $: setActive($activeSection);
 
   onMount(() => {
-    if ($activeSection === sectionIdx) {
-      searchInput.focus();
-    }
+    setActive($activeSection);
   });
 
 </script>
 
-<input type="text" bind:this={searchInput} />
+<input type="text" bind:this={searchInput} bind:value={searchTerm} />
 
 {#if keyboardActive}
   <div class="keyboard">
-    {#each keyLines as line}
+    {#each keyLines as line, rowIndex}
       <div class="keyboard__row">
-        {#each line as keyButton}
-          <button class="keyboard__btn">
+        {#each line as keyButton, colIndex}
+          <button class="keyboard__btn" class:keyboard__btn--active="{rowIndex === keyboardY && colIndex === keyboardX}">
             {keyButton}
           </button>
         {/each}
@@ -84,10 +132,18 @@
 
     &__btn {
       align-items: center;
+      background-color: rgba(0,0,0, 0.8);
+      color: $white;
       display: flex;
       height: 40px;
       justify-content: center;
+      transition: background-color, 0.4s;
       width: 40px;
+
+      &--active {
+        background-color: $white;
+        color: black;
+      }
     }
   }
 
